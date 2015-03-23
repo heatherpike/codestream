3bd3c179d23789b9d89722819290e1735e6e7557
crullian <crullian@gmail.com>
2015-03-23 13:12:40 -0400

    Clean up editor branch

--- student/public/javascripts/factories/TimelineFactory.js
+++
@@ -1,9 +0,0 @@
-app.factory('TimelineFactory', function($http) {
-  return {
-    getTimeline: function(cb) {
-      return $http.get('/commits').then(function(res) {
-        cb(res.data);
-      });
-    }
-  };
-})
