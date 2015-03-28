f23d5851daf1e467abdf68ef8dcbc0b3e96d370e
crullian <crullian@gmail.com>
2015-03-28 14:44:13 -0400

    Add sorting function to improve timeline sequence

--- fsgApp/browser/js/common/factories/TimeLineFactory.js
+++ fsgApp/browser/js/common/factories/TimeLineFactory.js
@@ -4,6 +4,17 @@ app.factory('TimelineFactory', function($http) {
       return $http.get('/api/timeline').then(function(res) {
         cb(res.data);
       });
+    },
+    sortByDate: function(commits) {
+      function compare(a, b) {
+        if (a.date < b.date)
+          return -1;
+        if (a.date > b.date)
+          return 1;
+        return 0;
+      }
+
+      return commits.sort(compare);
     }
   };
 })
