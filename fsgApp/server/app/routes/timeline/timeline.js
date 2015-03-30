026feef720e36d308eb6ba42626696d87e44c3f0
crullian <crullian@gmail.com>
2015-03-30 10:56:48 -0400

    Formatting the chatbox

--- fsgApp/server/app/routes/timeline/timeline.js
+++ fsgApp/server/app/routes/timeline/timeline.js
@@ -1,5 +1,4 @@
 var Promise = require('bluebird');
-
 var path = require('path');
 
 //Using this path for example
@@ -40,16 +39,16 @@ module.exports = (function(cb) {
           var commitsArr = commits.map(function(commit) {
             var count = 1;
             var commitObject = {
-              number: "", //"commit: " + count++,
+              number: '', //"commit: " + count++,
               id: commit.sha(),
               author: commit.author().name(),
               date: commit.date(),
               message: commit.message()
             };
             return commitObject;
-          })
+          });
           resolve(commitsArr);
-        })
+        });
 
         // Start emitting events.
         history.start();
