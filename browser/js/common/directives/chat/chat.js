9bc5d10fc281ca190878c83e30994c3d0475f9dd
crullian <crullian@gmail.com>
2015-04-01 19:42:03 -0400

    Overhaul color scheme

--- browser/js/common/directives/chat/chat.js
+++ browser/js/common/directives/chat/chat.js
@@ -1,9 +1,9 @@
 'use strict';
-app.directive('chat', function () {
+app.directive('chat', function() {
   return {
-      restrict: 'E',
-      templateUrl: 'js/common/directives/chat/chat.html',
-      controller: 'ChatCtrl'
+    restrict: 'E',
+    templateUrl: 'js/common/directives/chat/chat.html',
+    controller: 'ChatCtrl'
   };
 });
 
@@ -12,14 +12,14 @@ app.controller('ChatCtrl', function($scope, socket, Chat) {
     console.log("chat get", Chat.get);
     //will need to call Chat.get from the factory once we set up Chat message array in the model
     // Chat.get().then(function (repo) {
-      var repo = {
-          room: 'chat room',
-          messages: ['hey', 'how are you']
-        };
-      $scope.room = repo.room; 
-      $scope.messages = repo.messages;
-      console.log($scope.messages);
-      socket.emit('join', $scope.room); 
+    var repo = {
+      room: 'chat room',
+      messages: ['hey', 'how are you']
+    };
+    $scope.room = repo.room;
+    $scope.messages = repo.messages;
+    console.log($scope.messages);
+    socket.emit('join', $scope.room);
     // });
   };
 
@@ -31,14 +31,14 @@ app.controller('ChatCtrl', function($scope, socket, Chat) {
     console.log("submit called", message);
     //will need to call Chat.add from the factory to add this message to the messages array once we set up Chat message array in the model
     // Chat.add(message).then(function(data) {
-      socket.emit('send message', message);
-      $scope.chatInput = '';
+    socket.emit('send message', message);
+    $scope.chatInput = '';
     // });  
-  };    
+  };
 
   socket.on('new message', function(data) {
-      console.log("data", data);
-      $scope.messages.push(data);
+    console.log("data", data);
+    $scope.messages.push(data);
   });
 
 });
@@ -51,7 +51,7 @@ app.controller('ChatCtrl', function($scope, socket, Chat) {
 //               var $chat = $('#chat');
 
 //               socket.emit('join', 'juniors');
-              
+
 //               $messageForm.submit(function(e){
 //                 e.preventDefault();
 //                 var message = {};
@@ -61,7 +61,7 @@ app.controller('ChatCtrl', function($scope, socket, Chat) {
 //                 socket.emit('send message', message);
 //                 $messageBox.val('');
 //               });
-              
+
 //               socket.on('new message', function(data){
 //                 $chat.append(data + "<br/>");
 //               });
