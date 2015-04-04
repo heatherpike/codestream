f79a023c0ff17fe62c2309e335a159002175fb93
Richard Moore <Rmoore424@gmail.com>
2015-04-04 14:11:28 -0400

    changed github flow, should work on server now

--- server/db/models/repo.js
+++ server/db/models/repo.js
@@ -32,7 +32,7 @@ schema.method('createRemote', function (name, username, password) {
 	return deferred.promise;
 });
 
-schema.method('addHook', function (repoInfo, username, password) {
+schema.method('addHook', function (name, username, password) {
 	var deferred = Q.defer();
 
 	github.authenticate({
@@ -43,43 +43,42 @@ schema.method('addHook', function (repoInfo, username, password) {
 
 	github.repos.createHook({
 		user: username,
-		repo: repoInfo.name,
+		repo: name,
 		name: 'web',
 		config: {
-			url: 'http://codestream.co/api/cli/repos/' + this._id + '/push',
+			url: 'http://593d2949.ngrok.com/api/cli/repos/' + this._id + '/push',
 			content_type: 'application/json',
 			secret: 'codestream is awesome'
 		}
 	}, function (err, hookInfo) {
 		if (err) deferred.reject(err);
-		deferred.resolve(repoInfo);
+		deferred.resolve();
 	})
 	return deferred.promise;
 });
 
-schema.method('initialCommit', function (file, repo) {
-	var deferred = Q.defer();
-	var gitRepo = git(repo);
-	gitRepo.add(file, function (err) {
-		if (err) deferred.reject(err);
-		gitRepo.commit("auto committed by Codestream", function (err) {
-			if (err) deferred.reject(err);
-			gitRepo.remote_push('origin', 'master', function (err) {
-				if (err) deferred.reject(err);
-				deferred.resolve();
-			});
-		});
-	});
-	return deferred.promise;
-})
+// schema.method('initialCommit', function (file, repo) {
+// 	var deferred = Q.defer();
+// 	var gitRepo = git(repo);
+// 	gitRepo.add(file, function (err) {
+// 		if (err) deferred.reject(err);
+// 		gitRepo.commit("auto committed by Codestream", function (err) {
+// 			if (err) deferred.reject(err);
+// 			gitRepo.remote_push('origin', 'master', function (err) {
+// 				if (err) deferred.reject(err);
+// 				deferred.resolve();
+// 			});
+// 		});
+// 	});
+// 	return deferred.promise;
+// })
 
-schema.method('clone', function (repoInfo, repoId, username) {
+schema.method('clone', function (name, repoId, username) {
 	var deferred = Q.defer();
-
-	git.clone('git@github.com:'+username+'/'+repoInfo.name+'.git', 
-  '../repos/'+repoId, function(err, _repo) {
+	git.clone('git@github.com:'+username+'/'+name+'.git', 
+  './repos/'+repoId, function(err, _repo) {
 	    if(err) deferred.reject(err);
-	    deferred.resolve(repoInfo);  
+	    deferred.resolve(name);  
 	});
 	return deferred.promise;
 });
