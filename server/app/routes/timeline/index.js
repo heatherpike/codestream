62fd1154509d2f04994c5688c61c16a1e290c55d
Heather Pike <heatherlaurenpike@gmail.com>
2015-04-04 20:39:32 -0400

    cleaned up console logs in timeline files

--- server/app/routes/timeline/index.js
+++ server/app/routes/timeline/index.js
@@ -6,10 +6,7 @@ var mongoose = require('mongoose');
 router.get('/:dirname', function (req, res, next) {
 	console.log("timeline req", req.params);
 	mongoose.model('Repo').findOne({_id: req.params.dirname}).populate('user').exec(function(err, repo) {
-		console.log("repo is", repo)
 		timeline(req.params.dirname, function(commits) {
-		console.log("commits", commits);
-		console.log("latest", commits[0].id);
     	res.status(200).send({user: repo.user, repoName: repo.name, commits: commits});
 	})
   });
