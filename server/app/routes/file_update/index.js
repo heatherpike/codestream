73d2e4346525a5336a8ec767f787351d28a6bcca
Colin Meret <colinmeret@gmail.com>
2015-04-21 15:07:20 -0400

    fixed redirection of live file update to proper socket room

--- server/app/routes/file_update/index.js
+++ server/app/routes/file_update/index.js
@@ -6,16 +6,9 @@ var fs = require('fs');
 router.post('/', function(req, res) {
   var io = require('../../../io')();
   var file = req.body;
-  var codestream = req.body.folder + '/codestream.txt';
-  if (fs.existsSync(codestream)) {
-  	var contents = fs.readFileSync(codestream).toString();
-  	var repoId = contents.split(" ")[1];
-  	io.to(repoId).emit('file updated', file);
-  	res.sendStatus(200);
-  }
-  else {
-  	res.sendStatus(200);
-  }
+  var repoId = file.repoId;
+  io.to(repoId).emit('file updated', file);
+  res.sendStatus(200);
   // console.log("File changed at line number", file.line[0]);
   //will need to emit to the room similar to the chat messages (see io/index.js)
   //can get the room from req.body once we add room to the model
