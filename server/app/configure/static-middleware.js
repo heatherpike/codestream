34bb6c7b256d886aeaef815e5b0bfaa60e3e0c8d
crullian <crullian@gmail.com>
2015-05-30 20:17:49 -0400

    Change location where the demo video lives

--- server/app/configure/static-middleware.js
+++ server/app/configure/static-middleware.js
@@ -8,11 +8,13 @@ module.exports = function (app) {
     var root = app.getValue('projectRoot');
     var bowerPath = path.join(root, './bower_components');
     var publicPath = path.join(root, './public');
+    var assetsPath = path.join(root, './assets');
     var browserPath = path.join(root, './browser');
 
     app.use(favicon(app.getValue('faviconPath')));
     app.use(express.static(bowerPath));
     app.use(express.static(publicPath));
+    app.use(express.static(assetsPath));
     app.use(express.static(browserPath));
 
 };
